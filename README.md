97d09f645fc402d05ce554477048624ada1b627a
Sudharsan SundaraBabu <sudharsan.s@gmail.com>
2020-02-27 16:35:59 +0000

    feat: Added data driven behaviour

--- README.md
+++ README.md
@@ -15,18 +15,28 @@ This repository implements a simple soap server that uses express framwwork.
 ```
 
 # Test SOAP operations
-To call the SOAP operation `GetSubscriberInventoryId` issue the following command
+This mock server is data driven.<BR>
+Any request with MSISDN `07777777777` shall be responded with a success response.<BR>
+Any request with MSISDN `09999999999` shall be responded with a success response.<BR> 
+<BR>
+The above behaviour is coded in ./server/index.js. Feel free to change them.<BR>
+
+To call the SOAP operation `GetSubscriberInventoryId` with a happy case MSISDN issue the following command
+
 ```
 curl --location --request POST 'http://localhost:3000/GetSubscriberInventoryId' \
+--header 'Content-Type: application/atom+xml' \
+--data-raw '<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://ws.cdyne.com/"><soapenv:Header /><soapenv:Body><ws:GetSubscriberInventoryId><ws:SubscriberId>07777777777</ws:SubscriberId></ws:GetSubscriberInventoryId></soapenv:Body></soapenv:Envelope>'
+```
+
+To call the SOAP operation `GetSubscriberConnectionHistory` issue the following command
+
+```
+curl --location --request POST 'http://localhost:3000/GetSubscriberConnectionHistory' \
 --header 'Content-Type: application/xml' \
 --data-raw '<?xml version="1.0" encoding="UTF-8"?>
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://ws.cdyne.com/"><soapenv:Header /><soapenv:Body><ws:GetSubscriberInventoryId><ws:SubscriberId>07777722222</ws:SubscriberId></ws:GetSubscriberInventoryId></soapenv:Body></soapenv:Envelope>'
 ```
-
-To call the SOAP operation `GetSubscriberConnectionHistory` issue the following command
-```
-curl --location --request POST 'http://localhost:3000/GetSubscriberConnectionHistory' \
---header 'Content-Type: text/plain' \
---data-raw '<?xml version="1.0" encoding="UTF-8"?>
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://ws.cdyne.com/"><soapenv:Header /><soapenv:Body><ws:GetSubscriberInventoryId><ws:SubscriberId>07777722222</ws:SubscriberId></ws:GetSubscriberInventoryId></soapenv:Body></soapenv:Envelope>'
-```
+<BR>
+Alternatively, use postman collections that are available in this repository.
--- server/index.js
+++ server/index.js
@@ -1,19 +1,35 @@
 const express = require('express');
+var xmlparser = require('express-xml-bodyparser');
+
 const app = express();
 const port = 3000;
+app.use(xmlparser());
 
-const getSubscriberInventoryIdResponse       = '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Header><ns1:RequestHeader xmlns:ns1="https://www.google.com/apis/ads/publisher/v202002" soapenv:actor="http://schemas.xmlsoap.org/soap/actor/next" soapenv:mustUnderstand="0"><ns1:networkCode>123456</ns1:networkCode><ns1:applicationName>DfpApi-Java-2.1.0-dfp_test</ns1:applicationName></ns1:RequestHeader></soapenv:Header><soapenv:Body><getSubscriberInventoryIdResponse xmlns="https://www.google.com/apis/ads/publisher/v202002">VODA001</getSubscriberInventoryIdResponse></soapenv:Body></soapenv:Envelope>';
-const getSubscriberConnectionHistoryResponse = '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Header><ns1:RequestHeader xmlns:ns1="https://www.google.com/apis/ads/publisher/v202002" soapenv:actor="http://schemas.xmlsoap.org/soap/actor/next" soapenv:mustUnderstand="0"><ns1:networkCode>123456</ns1:networkCode><ns1:applicationName>DfpApi-Java-2.1.0-dfp_test</ns1:applicationName></ns1:RequestHeader></soapenv:Header><soapenv:Body><GetSubscriberConnectionHistoryResponse><ServiceType>PAYG</ServiceType><ListofServiceCharacteristics><Status>Connected</Status><Product>BROADBAND</Product></ListofServiceCharacteristics></GetSubscriberConnectionHistoryResponse></soapenv:Body></soapenv:Envelope>';
+const UNHAPPY_CASE_MSISDN = '09999999999';
+
+const getSubscriberInventoryIdHapppyResponse       = '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Header><ns1:RequestHeader xmlns:ns1="https://www.google.com/apis/ads/publisher/v202002" soapenv:actor="http://schemas.xmlsoap.org/soap/actor/next" soapenv:mustUnderstand="0"><ns1:networkCode>123456</ns1:networkCode><ns1:applicationName>DfpApi-Java-2.1.0-dfp_test</ns1:applicationName></ns1:RequestHeader></soapenv:Header><soapenv:Body><getSubscriberInventoryIdResponse xmlns="https://www.google.com/apis/ads/publisher/v202002">VODA001</getSubscriberInventoryIdResponse></soapenv:Body></soapenv:Envelope>';
+const getSubscriberInventoryIdErrorResponse        = '<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/1999/XMLSchema" xmlns:xsi="http://www.w3.org/1999/XMLSchema-instance"><SOAP-ENV:Body><SOAP-ENV:Fault><faultcode xsi:type="xsd:string">SOAP-ENV:Client</faultcode><faultstring xsi:type="xsd:string">Failed to fetch Inventory records</faultstring></SOAP-ENV:Fault></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+const getSubscriberConnectionHistoryHappyResponse = '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Header><ns1:RequestHeader xmlns:ns1="https://www.google.com/apis/ads/publisher/v202002" soapenv:actor="http://schemas.xmlsoap.org/soap/actor/next" soapenv:mustUnderstand="0"><ns1:networkCode>123456</ns1:networkCode><ns1:applicationName>DfpApi-Java-2.1.0-dfp_test</ns1:applicationName></ns1:RequestHeader></soapenv:Header><soapenv:Body><GetSubscriberConnectionHistoryResponse><ServiceType>PAYG</ServiceType><ListofServiceCharacteristics><Status>Connected</Status><Product>BROADBAND</Product></ListofServiceCharacteristics></GetSubscriberConnectionHistoryResponse></soapenv:Body></soapenv:Envelope>';
+const getSubscriberConnectionHistoryErrorResponse = '<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/1999/XMLSchema" xmlns:xsi="http://www.w3.org/1999/XMLSchema-instance"><SOAP-ENV:Body><SOAP-ENV:Fault><faultcode xsi:type="xsd:string">SOAP-ENV:Client</faultcode><faultstring xsi:type="xsd:string">No Connection history Exists for this Subscriber</faultstring></SOAP-ENV:Fault></SOAP-ENV:Body></SOAP-ENV:Envelope>';
 
 app.post('/GetSubscriberInventoryId', (req, res) => {
     res.set('Content-Type', 'text/xml');
-    res.send(getSubscriberInventoryIdResponse);
+    let body = JSON.stringify(req.body);
+    if (body.includes(UNHAPPY_CASE_MSISDN)) { 
+        res.send(getSubscriberInventoryIdErrorResponse);
+    } else {
+        res.send(getSubscriberInventoryIdHapppyResponse);
+    }
 });
 
 app.post('/GetSubscriberConnectionHistory', (req, res) => {
     res.set('Content-Type', 'text/xml');
-    res.send(getSubscriberConnectionHistoryResponse);
-
+    let body = JSON.stringify(req.body);
+    if (body.includes(UNHAPPY_CASE_MSISDN)) { 
+        res.send(getSubscriberConnectionHistoryErrorResponse);
+    } else {
+        res.send(getSubscriberConnectionHistoryHappyResponse);
+    }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
